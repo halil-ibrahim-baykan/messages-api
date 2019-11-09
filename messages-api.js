ea840124763d7451eb8b700c45158cc07deb6ed2
halil-ibrahim-baykan <halilibrahimbaykan@gmail.com>
2019-11-09 17:00:30 +0100

    finish, hope correct

--- messages-api.js
+++ messages-api.js
@@ -7,14 +7,30 @@ const app = express();
 const port = 3000;
 const corsMiddleware = cors();
 const jsonParser = bodyParser.json();
+0;
+
+let count = 0;
+
+const countMiddleware = (req, res, next) => {
+  if (count > 5) {
+    res.status(429).send("Too Many Requests");
+  } else {
+    next();
+  }
+  count++;
+};
 
 app.use(corsMiddleware);
 app.use(jsonParser);
 app.use(movieRouter);
 
-app.post("/messages", (req, res) => {
+app.post("/messages", countMiddleware, (req, res) => {
   console.log(req.body);
-  res.json("message: Messaged receive loud and clear");
+  if (!req.body.text || req.body.text === "") {
+    res.status(400).send("Bad Request");
+  } else {
+    res.status(201).json("message: Messaged received loud and clear");
+  }
 });
 
 app.listen(port, () => console.log("Server is running on port: " + port));
